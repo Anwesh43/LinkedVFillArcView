332ef3671b802ddf59d3bb76544ea964686d8498
Anwesh43 <anweshthecool0@gmail.com>
2021-04-18 03:47:59 +0530

    adding VFANode class

--- vfillarcview/src/main/java/com/example/vfillarcview/VFillArcView.kt
+++ vfillarcview/src/main/java/com/example/vfillarcview/VFillArcView.kt
@@ -119,4 +119,45 @@ class VFillArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class VFANode(var i : Int, val state : State = State()) {
+
+        private var next : VFANode? = null
+        private var prev : VFANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = VFANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawVFANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : VFANode {
+            var curr : VFANode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
