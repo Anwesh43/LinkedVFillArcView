6361e2fe90bc90694fbf771738259eab905890ea
Anwesh43 <anweshthecool0@gmail.com>
2021-04-18 02:38:10 +0530

    adding Int and Float extension methofs

--- vfillarcview/src/main/java/com/example/vfillarcview/VFillArcView.kt
+++ vfillarcview/src/main/java/com/example/vfillarcview/VFillArcView.kt
@@ -24,3 +24,8 @@ val sizeFactor : Float = 2.9f
 val parts : Int = 3
 val deg : Float = 90f
 val scGap : Float = 0.02f / parts
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
