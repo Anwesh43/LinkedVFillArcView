4ffacd86319e1932e452064d4b60e5761e3dad78
Anwesh43 <anweshthecool0@gmail.com>
2021-04-18 03:56:46 +0530

    adding fullscreen support

--- app/src/main/java/com/example/linkedvfillarcview/MainActivity.kt
+++ app/src/main/java/com/example/linkedvfillarcview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedvfillarcview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.vfillarcview.VFillArcView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        VFillArcView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
